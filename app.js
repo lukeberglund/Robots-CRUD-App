c0aeb327ab42667969d108b485e08e36287fe208
lukeberglund <berglundl1@southernct.edu>
2017-05-06 18:53:31 -0400

    icon

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app/use('/', robots);
+app.use('/', robots);
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
