8562b3ad4c6f7c7486eb7b9c22257f1be1e852eb
lukeberglund <berglundl1@southernct.edu>
2017-05-10 22:54:52 -0400

    commitAgain

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Robot CRUD App' });
+  res.render('index', { title: 'My Robots CRUD App' });
 });
 
 module.exports = router;
